fce1b66e255bb749d22baf2a03eff46dd6b39ae7
redaProg-wq <rdamekouar@gmail.com>
2023-12-07 09:00:02 +0100

    celine

---
+++ includes/header.php
@@ -0,0 +1,19 @@
+<header class="header container-fluid d-flex flex-row">
+      <h1 class="headerh1"> <a class="fontdancing" href="#">
+        Céline <br />
+        Magnétisme et Voyance </a>
+      </h1>
+      
+      
+      <nav class="nav">
+        <ul class=" d-flex  flex-row justify-content-evenly" >    
+       <li> <a class="nav-link active" aria-current="page" href="./index.html">Celine</a></li>
+       <li> <a class="nav-link" href="./Our_studio.html">Voyance</a></li>
+       <li> <a class="nav-link" href="./RCMResults.html">Magnetisme</a></li>
+       <li> <a class="nav-link" href="./competitionresults.html">Questions</a></li>
+       <li> <a class="nav-link" href="./videos.html">Témoignages</a></li>
+       <li> <a class="nav-link" href="./contacts.html">Contacts</a></li>
+        </ul>
+      </nav>
+
+</header>
---
+++ index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<?php require './includes/head.php' ?>
+
+
+
+<body>
+
+    <?php require './includes/header.php' ?>
+
+    <div class="accueil_premier container-fluid ">
+
+        <div class="container-fluid d-flex  justify-content-around row pt-5">
+
+
+            
+            <div class="txt_acceuil container-fluid d-flex flex-column align-items-center col-10">
+                <h2 class="fontdancing text-center"> Céline, Voyante et Magnetiseuse</h2>
+                <p class="text-center">A Six-Fours, dans le Var, et Partout dans le monde</p>
+                <button class="button-89 mt-1"> En savoir plus</button>
+            </div>
+
+            <div class="txt_acceuil container-fluid d-flex flex-column align-items-center col-10">
+                <h2 class="fontdancing text-center"> Présentations</h2>
+                <p class="text-center">Bonjour à toutes et tous. <br> Je suis Céline ...</p>
+                <button class="button-89 mt-1"> En savoir plus</button>
+            </div>
+
+            <div class="txt_acceuil container-fluid d-flex flex-column align-items-center col-10">
+                <h2 class="fontdancing text-center">Services de Voyance</h2>
+                <p class="text-center">A Six-Fours, dans le Var, et Partout dans le monde</p>
+                <button class="button-89 mt-1"> En savoir plus</button>
+            </div>
+
+            <div class="txt_acceuil container-fluid d-flex flex-column align-items-center col-10">
+                <h2 class="fontdancing text-center">Services de Magnetisme </h2>
+                <p class="text-center">A Six-Fours, dans le Var, et Partout dans le monde</p>
+                <button class="button-89 mt-1"> En savoir plus</button>
+            </div>
+
+            <div class="txt_acceuil container-fluid d-flex flex-column align-items-center col-10">
+                <h2 class="fontdancing text-center">Contacts </h2>
+                <p class="text-center">A Six-Fours, dans le Var, et Partout dans le monde</p>
+                <button class="button-89 mt-1"> En savoir plus</button>
+            </div>
+
+        </div>
+
+
+
+    </div>
+
+
+
+
+    <?php require './includes/footer.php' ?>
+</body>
+
+</html>
